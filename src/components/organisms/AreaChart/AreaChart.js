f85afe8bc25d64e1fba0c044f18fa8e5cfddc5e8
Sajjad Haider <sani723@hotmail.com>
2024-08-01 15:42:54 +0400

    Add the AreaChart component

---
+++ src/components/organisms/AreaChart/AreaChart.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const data = [
+  { name: "Jan", uv: 4000, pv: 2400, amt: 2400 },
+  { name: "Feb", uv: 3000, pv: 1398, amt: 2210 },
+  { name: "Mar", uv: 2000, pv: 9800, amt: 2290 },
+  { name: "Apr", uv: 2780, pv: 3908, amt: 2000 },
+  { name: "May", uv: 1890, pv: 4800, amt: 2181 },
+  { name: "Jun", uv: 2390, pv: 3800, amt: 2500 },
+  { name: "Jul", uv: 3490, pv: 4300, amt: 2100 },
+];
+
+const CustomAreaChart = ({
+  width = "100%",
+  height = 400,
+  dataKey = "uv",
+  stroke = "#8884d8",
+  fill = "#8884d8",
+}) => (
+  <ResponsiveContainer width={width} height={height}>
+    <AreaChart data={data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Area type="monotone" dataKey={dataKey} stroke={stroke} fill={fill} />
+    </AreaChart>
+  </ResponsiveContainer>
+);
+
+export default CustomAreaChart;
